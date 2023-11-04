4954b5e753542bc203f1b59b79b326582f60027b
kjh0work_local <rlawogns0513@naver.com>
2023-11-04 15:27:18 +0900

    CreateNewProject

---
+++ EssentialNews/src/main/java/com/example/essentialnews/EssentialNewsApplication.java
@@ -0,0 +1,13 @@
+package com.example.essentialnews;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EssentialNewsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EssentialNewsApplication.class, args);
+	}
+
+}
---
+++ EssentialNews/src/test/java/com/example/essentialnews/EssentialNewsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.essentialnews;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EssentialNewsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
